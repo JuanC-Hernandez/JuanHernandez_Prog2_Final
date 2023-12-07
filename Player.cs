5a29e4bdeda93cf68b14a52a1b87eaf5ebe7e3ab
Juan_C_Hernandez <33169379+JuanC-Hernandez@users.noreply.github.com>
2023-12-06 23:40:15 -0800

    Player class

---
+++ Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuanHernandez_Prog2_Final
+{
+    public class Player
+    {
+        // Fields
+        string _fname;
+        string _lname;
+        string _team;
+        string _status;
+
+        //constructor
+        public Player(string fname, string lname, string team, string status)
+        {
+            _fname = fname;
+            _lname = lname; 
+            _team = team;
+            _status = status;
+        }// Player constructor
+
+        // properties
+        public string Fname { get => _fname; set => _fname = value; }
+        public string Lname { get => _lname; set => _lname = value; }
+        public string Team { get => _team; set => _team = value; }
+        public string Status { get => _status; set => _status = value; }
+    }// Player
+}// namespace
